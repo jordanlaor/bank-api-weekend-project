af342ec93b3aca208e9cff1a7e2618b50524d1ed
Jordan Laor <jordanlaor@gmail.com>
2021-04-15 11:58:27 +0300

    starting the project

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("listening on port 3000..."));
