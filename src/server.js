8bb3ff9d12b384d3c753562a60b9df31ea02422c
Jordan Laor <jordanlaor@gmail.com>
2021-04-15 12:45:10 +0300

    added comments (pseudo code)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const express = require("express");
+const {} = require("./utils");
 
 const app = express();
 app.use(express.json());
+
+// get all users
+app.get("/api/users", async (req, res) => {});
+
+// add a user
+app.post("/api/users", async (req, res) => {});
+
+// deposit money
+// update credit
+// withdraw money
+// transfer money
+// user by passport
+// get all users
 
 const PORT = 3000;
 app.listen(PORT, () => console.log("listening on port 3000..."));
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+// add a user
+// deposit money
+// update credit
+// withdraw money
+// transfer money
+// user by passport
+// all users
