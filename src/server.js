067339b8277d6af220b53692ecb91bed06225871
Jordan Laor <jordanlaor@gmail.com>
2021-04-16 18:24:08 +0300

    api ready

--- src/server.js
+++ src/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
-const { getAllUsers, getUserByPassport, createUser, depositMoney } = require("./utils");
+const {
+  getAllUsers,
+  getUserByPassport,
+  createUser,
+  depositMoney,
+  withdrawMoney,
+  transferMoney,
+  updateCredit,
+  changeActiveStatus,
+  filterWithQuery,
+  filterWithParams,
+} = require("./utils");
 
 const app = express();
 app.use(express.json());
@@ -34,11 +45,15 @@ app.post("/api/users", async (req, res) => {
   }
 });
 
-// activate a user
-app.put("/api/users/:passport/activate");
-
-// deactivate a user
-app.put("/api/users/:passport/deactivate");
+// activate/deactivate a user
+app.put("/api/account/:passport/status", async (req, res) => {
+  try {
+    const user = await changeActiveStatus(req.params.passport, req.body.status);
+    res.status(200).send({ ...user });
+  } catch (error) {
+    res.status(error.code).send({ message: error.message });
+  }
+});
 
 // deposit money
 app.put("/api/account/:passport/deposit", async (req, res) => {
@@ -51,13 +66,56 @@ app.put("/api/account/:passport/deposit", async (req, res) => {
 });
 
 // update credit
-app.put("/api/account/:passport/credit");
+app.put("/api/account/:passport/credit", async (req, res) => {
+  try {
+    const user = await updateCredit(req.params.passport, req.body.credit);
+    res.status(200).send({ ...user });
+  } catch (error) {
+    res.status(error.code).send({ message: error.message });
+  }
+});
 
 // withdraw money
-app.put("/api/account/:passport/withdraw");
+app.put("/api/account/:passport/withdraw", async (req, res) => {
+  try {
+    const user = await withdrawMoney(req.params.passport, req.body.amount);
+    res.status(200).send({ ...user });
+  } catch (error) {
+    res.status(error.code).send({ message: error.message });
+  }
+});
 
 // transfer money
-app.put("/api/account/transfer/:from/:to");
+app.put("/api/account/transfer/:from/:to", async (req, res) => {
+  try {
+    const users = await transferMoney(req.params.to, req.params.from, req.body.amount);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(error.code).send({ message: error.message });
+  }
+});
+
+// filter with query
+app.get("/api/filter/query", async (req, res) => {
+  try {
+    const { query, ...fields } = req.query;
+    const users = await filterWithQuery(query, fields);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(error.code).send({ message: error.message });
+  }
+});
+
+// filter with params
+app.get("/api/filter/params", async (req, res) => {
+  try {
+    const users = await filterWithParams(req.query);
+    res.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+    res.status(error.code).send({ message: error.message });
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log("listening on port 3000..."));
--- src/utils.js
+++ src/utils.js
@@ -2,17 +2,13 @@ const fs = require("fs").promises;
 
 const USERS_JSON = `${__dirname}/bank.json`;
 
-// update credit
-// withdraw money
-// transfer money
-
 // get all users
 const getAllUsers = async () => {
   try {
     const users = await fs.readFile(USERS_JSON, "utf-8");
     return users || JSON.stringify([]);
   } catch (error) {
-    throw { message: error.code, code: 500 };
+    throw { message: error.code, code: error.code || 500 };
   }
 };
 
@@ -25,7 +21,7 @@ const getUserByPassport = async (passport) => {
     if (user) return user;
     throw { message: "no such user was found", code: 404 };
   } catch (error) {
-    throw { message: error.message, code: 500 };
+    throw { message: error.message, code: error.code || 500 };
   }
 };
 
@@ -51,18 +47,21 @@ const createUser = async ({ passport, isActive = true, credit = 0, cash = 0 }) =
       throw { message: error.message, code: 500 };
     }
   } catch (error) {
-    throw { message: error.message, code: 500 };
+    throw { message: error.message, code: error.code || 500 };
   }
 };
 
 // deposit money
 const depositMoney = async (passport, amount) => {
   if (!passport) throw { message: "you need to pass a passport id", code: 406 };
+  if (!amount) throw { message: "you need to pass an amount to deposit", code: 406 };
   amount = Number(amount);
-  if (Number.isNaN(amount)) throw { message: `amount needs to be a number`, code: 406 };
+  if (Number.isNaN(amount) || amount <= 0) throw { message: `amount needs to be a positive number`, code: 406 };
   try {
     const users = JSON.parse(await getAllUsers());
     const user = users.find((user) => String(user.passport) === String(passport));
+    if (!user) throw { message: "no such user was found", code: 404 };
+    if (!user.isActive) throw { message: "user's account is not active, you can't deposit money to the account", code: 400 };
     user.cash += amount;
     try {
       await fs.writeFile(USERS_JSON, JSON.stringify(users));
@@ -71,8 +70,153 @@ const depositMoney = async (passport, amount) => {
       throw { message: error.message, code: 500 };
     }
   } catch (error) {
-    throw { message: error.message, code: 500 };
+    throw { message: error.message, code: error.code || 500 };
   }
 };
 
-module.exports = { getAllUsers, getUserByPassport, createUser, depositMoney };
+// withdraw money
+const withdrawMoney = async (passport, amount) => {
+  if (!passport) throw { message: "you need to pass a passport id", code: 406 };
+  if (!amount) throw { message: "you need to pass an amount to deposit", code: 406 };
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) throw { message: `amount needs to be a positive number`, code: 406 };
+  try {
+    const users = JSON.parse(await getAllUsers());
+    const user = users.find((user) => String(user.passport) === String(passport));
+    if (!user) throw { message: "no such user was found", code: 404 };
+    if (!user.isActive) throw { message: "user's account is not active, you can't withdraw money from the account", code: 400 };
+
+    const expectedCashLeft = user.cash - amount;
+    if (expectedCashLeft < -user.credit) throw { message: "there is not enough cash in the account", code: 400 };
+    user.cash = expectedCashLeft;
+    try {
+      await fs.writeFile(USERS_JSON, JSON.stringify(users));
+      return user;
+    } catch (error) {
+      throw { message: error.message, code: 500 };
+    }
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+// transfer money
+const transferMoney = async (to, from, amount) => {
+  if (!to || !from) throw { message: "you need to pass a passport id for both giving and receiving accounts", code: 406 };
+  if (!amount) throw { message: "you need to pass an amount to deposit", code: 406 };
+  amount = Number(amount);
+  if (Number.isNaN(amount) || amount <= 0) throw { message: `amount needs to be a positive number`, code: 406 };
+  try {
+    const users = JSON.parse(await getAllUsers());
+    const giver = users.find((user) => String(user.passport) === String(from));
+    const receiver = users.find((user) => String(user.passport) === String(to));
+    if (!receiver || !giver) throw { message: "one of the passport ids is invalid", code: 404 };
+    if (!receiver.isActive || !giver.isActive)
+      throw { message: "one of the accounts is not active, you can't go through with the transfer", code: 400 };
+
+    const expectedCashLeft = giver.cash - amount;
+    if (expectedCashLeft < -giver.credit) throw { message: "there is not enough cash in the giving account", code: 400 };
+    giver.cash = expectedCashLeft;
+    receiver.cash += amount;
+    try {
+      await fs.writeFile(USERS_JSON, JSON.stringify(users));
+      return { receiver, giver };
+    } catch (error) {
+      throw { message: error.message, code: 500 };
+    }
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+// update credit
+const updateCredit = async (passport, newCredit) => {
+  if (!passport) throw { message: "you need to pass a passport id", code: 406 };
+  if (typeof newCredit === undefined) throw { message: "you need to pass a credit amount to update", code: 406 };
+  newCredit = Number(newCredit);
+  if (Number.isNaN(newCredit) || newCredit < 0) throw { message: `credit needs to be a positive number or zero`, code: 406 };
+  try {
+    const users = JSON.parse(await getAllUsers());
+    const user = users.find((user) => String(user.passport) === String(passport));
+    if (!user) throw { message: "no such user was found", code: 404 };
+    if (!user.isActive) throw { message: "user's account is not active, you can't change it's credit", code: 400 };
+    user.credit = newCredit;
+    try {
+      await fs.writeFile(USERS_JSON, JSON.stringify(users));
+      return user;
+    } catch (error) {
+      throw { message: error.message, code: 500 };
+    }
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+// activate/deactivate
+const changeActiveStatus = async (passport, status) => {
+  if (!passport) throw { message: "you need to pass a passport id", code: 406 };
+  if (typeof status === undefined) throw { message: "you need to pass a status", code: 406 };
+  status = Boolean(status);
+  try {
+    const users = JSON.parse(await getAllUsers());
+    const user = users.find((user) => String(user.passport) === String(passport));
+    if (!user) throw { message: "no such user was found", code: 404 };
+    user.isActive = status;
+    try {
+      await fs.writeFile(USERS_JSON, JSON.stringify(users));
+      return user;
+    } catch (error) {
+      throw { message: error.message, code: 500 };
+    }
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+const filterWithQuery = async (query, fields) => {
+  if (typeof query === "undefined" || !fields) throw { message: "you need to pass a query and the fields to search in", code: 406 };
+  try {
+    const users = JSON.parse(await getAllUsers());
+    const filterUsers = users.filter((user) => {
+      for (const field in fields) {
+        if (String(user[field]).includes(String(query))) return true;
+      }
+      return false;
+    });
+    console.log(filterUsers);
+    if (filterUsers.length) return filterUsers;
+    throw { message: "no user was found", code: 404 };
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+const filterWithParams = async (fields) => {
+  if (!fields) throw { message: "you need to pass the fields to search in", code: 406 };
+  try {
+    const users = JSON.parse(await getAllUsers());
+    let filterUsers = [...users];
+    for (const field in fields) {
+      filterUsers = filterUsers.filter((user) => {
+        return String(user[field]).includes(String(fields[field]));
+      });
+    }
+    if (filterUsers.length) return filterUsers;
+    throw { message: "no user was found", code: 404 };
+  } catch (error) {
+    throw { message: error.message, code: error.code || 500 };
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserByPassport,
+  createUser,
+  depositMoney,
+  withdrawMoney,
+  transferMoney,
+  updateCredit,
+  changeActiveStatus,
+  filterWithQuery,
+  filterWithParams,
+};
